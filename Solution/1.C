45f2e0062d70af963b4c268494c3732cc76f4851
Laxman sirvi <100264240+21BCA90@users.noreply.github.com>
2022-04-04 13:12:34 +0530

    1

---
+++ Solution/1.C
@@ -0,0 +1,17 @@
+/* WAP to orint hello world !.
+Name : laxman sirvi
+Date : 04-04-22*/
+#include<stdio.h>
+#include<conio.h>
+void hello();
+void main()
+{
+	clrscr();
+	hello();
+	getch();
+}
+void hello()
+{
+	printf("Hello world !");
+}
+
